303f50d22402a03d1208dc163d04031954447500
Anth <sol9001@gmail.com>
2023-11-19 22:59:13 -0500

    refactor

--- robodog/src/FileService.js
+++ robodog/src/FileService.js
@@ -1,61 +1,70 @@
 import FormatService from './FormatService';
 
-// Define your extractMarkdownContent function
-function extractMarkdownContent(file) {
-  return new Promise((resolve, reject) => {
-    const decoder = new TextDecoder('utf-8');
-    const text = decoder.decode(file);
-    resolve(text);
-  });
+// Extracts content from a text or markdown file
+function extractTextContent(arrayBuffer) {
+  const decoder = new TextDecoder('utf-8');
+  return decoder.decode(arrayBuffer);
 }
 
-// ... Rest of your code ...
+// Extracts content from a PDF file
+async function extractPDFContent(arrayBuffer) {
+  // Placeholder for actual PDF extraction logic
+  // Replace this with your actual PDF content extraction method
+}
 
+// Handles file input change event
+async function handleFileInputChange(fileInput, resolve, reject) {
+  const file = fileInput.files[0];
+
+  if (!file) {
+    reject('No file selected');
+    return;
+  }
+
+  const reader = new FileReader();
+
+  reader.onload = async (event) => {
+    try {
+      const arrayBuffer = event.target.result;
+
+      switch (file.type) {
+        case 'application/pdf':
+          try {
+            const pdfText = await extractPDFContent(arrayBuffer);
+            resolve(pdfText);
+          } catch (error) {
+            reject('Error processing PDF content');
+          }
+          break;
+        case 'text/plain':
+        case 'text/markdown':
+          resolve(extractTextContent(arrayBuffer));
+          break;
+        default:
+          reject('Invalid file format. Please select a PDF, text, or markdown file.');
+      }
+    } catch (error) {
+      reject('Error processing file content');
+    }
+  };
+
+  reader.onerror = () => reject('Error reading file');
+  reader.readAsArrayBuffer(file);
+}
+
+// Main function to extract content from file
 async function extractFileContent(setContent, content) {
   const fileInput = document.createElement('input');
   fileInput.type = 'file';
   fileInput.accept = '.md, .txt, .pdf';
+
   setContent([
     ...content,
     FormatService.getMessageWithTimestamp(fileInput.name, 'assistant')
   ]);
 
   return new Promise((resolve, reject) => {
-    fileInput.addEventListener('change', async () => {
-      const file = fileInput.files[0];
-      const reader = new FileReader();
-
-      reader.onload = async (event) => {
-        const content = event?.target?.result;
-        if (file.type === 'application/pdf') {
-          try {
-            const pdfText = await extractPDFContent(content);
-            resolve(pdfText);
-          } catch (error) {
-            reject('Error processing PDF content');
-          }
-        } else if (file.type === 'text/plain') {
-          const text = extractTextContent(content);
-          resolve(text);
-        } else if (file.name.endsWith('.md') || file.type === 'text/markdown') {
-          const markdownText = await extractMarkdownContent(content);
-          resolve(markdownText);
-        } else {
-          reject('Invalid file format. Please select a PDF, text, or markdown file.');
-        }
-      };
-
-      reader.onerror = () => {
-        reject('Error reading file');
-      };
-
-      if (file.type === 'application/pdf' || file.type === 'text/plain' || file.type === 'text/markdown') {
-        reader.readAsArrayBuffer(file);
-      } else {
-        reject('Invalid file format. Please select a PDF, text, or markdown file.');
-      }
-    });
-
+    fileInput.addEventListener('change', () => handleFileInputChange(fileInput, resolve, reject));
     fileInput.click();
   });
 }
