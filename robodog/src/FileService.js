4f370c3a117b3eb631cccb2d73dddabe7e07f8d3
Anthony Dourish <adourish@reisystems.com>
2023-11-21 08:17:05 -0500

    import multiple files.

--- robodog/src/FileService.js
+++ robodog/src/FileService.js
@@ -10,74 +10,83 @@ async function extractPDFContent(arrayBuffer) {
   // Replace this with your actual PDF content extraction method
 }
 const fileFormats = '.md, .txt, .pdf, .js, .cs, .java, .py, json, .yaml, .php';
-async function handleFileInputChange(fileInput, resolve, reject) {
-  const file = fileInput.files[0];
+async function handleFileInputChange(fileInput) {
+  const files = fileInput.files;
+  const fileCount = files.length;
+  let importedCount = 0;
+  let errorCount = 0;
+  let resultText = "Start importing " + fileCount + " files...\n";
 
-  if (!file) {
-    reject('No file selected');
-    return;
-  }
-
-  var reader = null;
-  try {
-    reader = new FileReader();
-
-    reader.onload = async (event) => {
-      try {
-        const arrayBuffer = event.target.result;
-        console.error(file);
-        switch (file.type) {
-          case 'application/pdf':
-            try {
-              const pdfText = await extractPDFContent(file.name, arrayBuffer);
-              resolve(pdfText);
-            } catch (error) {
-              reject('Error processing PDF content');
-            }
-            break;
-          case 'text/plain':
-          case 'text/markdown':
-            var t = extractTextContent(arrayBuffer);
-            resolve(file.name + ":\n" + t);
-            break;
-          default:
-            console.error(file);
-            if (file.name.endsWith('.md') || file.name.endsWith('.txt') || file.name.endsWith('.pdf') || file.name.endsWith('.js') || file.name.endsWith('.cs') || file.name.endsWith('.java') || file.name.endsWith('.py') || file.name.endsWith('.json') || file.name.endsWith('.yaml') || file.name.endsWith('.php')) {
-              var t = extractTextContent(arrayBuffer);
-              resolve(file.name + ":\n" + t);
-            } else {
-              reject('Invalid file format. Please select a supported file format.');
-            }
-
-        }
-      } catch (error) {
-        reject('Error processing file content');
+  for (let i = 0; i < fileCount; i++) {
+    const file = files[i];
+    try {
+      const arrayBuffer = await readFile(file);
+      resultText += "\n\nFile name: " + file.name + "\n";
+      switch (file.type) {
+        case 'application/pdf':
+          try {
+            const pdfText = await extractPDFContent(file.name, arrayBuffer);
+            resultText += pdfText;
+            importedCount++;
+          } catch (error) {
+            resultText += "Error processing PDF content: " + error;
+            errorCount++;
+          }
+          break;
+        case 'text/plain':
+        case 'text/markdown':
+          const textContent = extractTextContent(arrayBuffer);
+          resultText += file.name + ":\n" + textContent;
+          importedCount++;
+          break;
+        default:
+          if (isSupportedFileFormat(file.name)) {
+            const textContent = extractTextContent(arrayBuffer);
+            resultText += file.name + ":\n" + textContent;
+            importedCount++;
+          } else {
+            resultText += "Invalid file format for " + file.name;
+            errorCount++;
+          }
       }
-    };
-
-    reader.onerror = () => {
-      console.error(reader.error);
-      reject('Error reading file');
-    };
-
-    reader.readAsArrayBuffer(file);
-  } catch (ex) {
-    console.error(ex);
+    } catch (error) {
+      resultText += file.name + ": " + error;
+      errorCount++;
+    }
   }
+
+  resultText += "\n\nImported " + importedCount + " files successfully.";
+  resultText += "\nFailed to import " + errorCount + " files.";
+
+  return resultText;
+}
+
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => resolve(event.target.result);
+    reader.onerror = (error) => reject(error);
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+function isSupportedFileFormat(fileName) {
+  const supportedFormats = ['.md', '.txt', '.pdf', '.js', '.cs', '.java', '.py', '.json', '.yaml', '.php'];
+  return supportedFormats.some((format) => fileName.endsWith(format));
 }
 
 async function extractFileContent(setContent, content) {
   const fileInput = document.createElement('input');
   fileInput.type = 'file';
   fileInput.accept = fileFormats;
-
+  fileInput.multiple = true;
   setContent([
     ...content,
     FormatService.getMessageWithTimestamp(fileInput.name, 'assistant')
   ]);
 
   return new Promise((resolve, reject) => {
-    fileInput.addEventListener('change', () => handleFileInputChange(fileInput, resolve, reject));
+    fileInput.addEventListener('change', () => handleFileInputChange(fileInput).then(resolve).catch(reject));
     fileInput.click();
   });
 }
