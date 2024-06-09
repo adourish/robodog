cda9f63d843dfed4e319fdaf403aa50a5bedb6af
Anth <sol9001@gmail.com>
2024-06-09 14:59:49 -0400

    added dall-e support

--- robodog/src/ConsoleService.js
+++ robodog/src/ConsoleService.js
@@ -545,17 +545,19 @@ async function sendMessageToOpenAI(text, model, context, knowledge, completionTy
   }
 
   const handleDalliRestCompletion = async () => {
+    var size = "1024x1024";
+    var p3 = {
+      model: "dall-e-3",
+      prompt: text,
+      size: size, 
+      quality: "standard",
+      n: 1
+    };
 
-    response3 = openai.images.generate(
-      model = "dall-e-3",
-      prompt = text,
-      size = "1024x1024",
-      quality = "standard",
-      n = 1,
-    );
+    response3 = openai.images.generate(p3);
 
-    if (response) {
-      var image_url = response.data[0].url;
+    if (response3) {
+      var image_url = response3.data[0].url;
       _content = image_url;
       setMessage(image_url);
       var _c = [
