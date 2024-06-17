d1ea662aa8055ba5bfd101cf91dcfa0a836b902b
Anth <sol9001@gmail.com>
2024-06-16 20:16:24 -0400

    android

---
+++ robodogandroid/app/src/main/java/com/unclebulgaria/robodog/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.unclebulgaria.robodog
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val mWebView = findViewById<WebView>(R.id.webview)
+        val webSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.allowContentAccess = true
+        webSettings.domStorageEnabled = true
+        
+
+        mWebView.loadUrl("https://adourish.github.io/robodog/robodog/dist/")
+    }
+}
