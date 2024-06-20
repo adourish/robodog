850d983915e33f32db0d558b47d6531dc357435b
Anth <sol9001@gmail.com>
2024-06-19 21:34:49 -0400

    bug fixes

--- robodog/src/Console.jsx
+++ robodog/src/Console.jsx
@@ -604,11 +604,7 @@ function Console() {
             ))}
           </select>
         </div>
-        <div className="input-area">
-          <label for="uploader" class="label-uploader">📷</label>
-          <input type="file" id="uploader" multiple onChange={handleOCRUpload} style="display: none;" />
-          <button type="button" onClick={handleFileUpload} aria-label="file" className="file-button">📜</button>
-        </div>
+
         <div className="console-content">
           {content.map((item, index) => {
             if (item.role === 'image') {
