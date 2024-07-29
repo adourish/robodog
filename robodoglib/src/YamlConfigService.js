109914605a51fab6037e60721c6f8c8a0287a1ef
Anthony Dourish <adourish@reisystems.com>
2024-07-29 15:55:28 -0400

    added about

--- robodoglib/src/YamlConfigService.js
+++ robodoglib/src/YamlConfigService.js
@@ -32,38 +32,42 @@ configs:
       model: gpt-4
       stream: true
       specialist: nlp
+      about: best for performance 
     - provider: openAI
       model: gpt-3.5-turbo
       stream: true
       specialist: nlp
+      about: best for most questions
     - provider: llamaAI
       model: llama3-70b
       stream: false
       specialist: nlp
+      about: best for big content
     - provider: openAI
       model: gpt-4o
       stream: true
       specialist: nlp
+      about: best for summerizing
     - provider: openAI
       model: gpt-4-turbo
       stream: true
       specialist: nlp
-    - provider: openAI
-      model: gpt-3.5-turbo
-      stream: true
-      specialist: nlp
+      about: best for speed
     - provider: openAI
       model: gpt-3.5-turbo-16k
       stream: true
       specialist: nlp
+      about: best for large docs when speed is not an issue
     - provider: openAI
       model: dall-e-3
       stream: false
       specialist: gi
+      about: best for creating images
     - provider: searchAPI
       model: search
       stream: false
       specialist: search
+      about: best for searching
       
       `
       return yamlstring;
